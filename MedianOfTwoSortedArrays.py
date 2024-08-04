2b3772c894f0ed62881c9d4fca7ab1a3ca9d7caf
harshtiwari1810 <harshtiwari1810@gmail.com>
2024-08-04 22:46:47 +0530

    dead com 26

--- MedianOfTwoSortedArrays.py
+++ MedianOfTwoSortedArrays.py
@@ -6,6 +6,7 @@ class Solution:
         if (len(mergeList)%2 != 0):
             mid = len(mergeList)//2
             return mergeList[mid]
+            
         else:
             mid = int(len(mergeList)/2)
             ans = (mergeList[mid] + mergeList[mid-1])/2
