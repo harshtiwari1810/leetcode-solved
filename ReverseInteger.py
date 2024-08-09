f4f55e307abc7ae254d06db74fbb1a70700431cb
harshtiwari1810 <harshtiwari1810@gmail.com>
2024-08-09 23:15:20 +0530

    dead com

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -2,6 +2,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         if x <= -2**31 or x >= (2**31)-1 or x == 0:
             return 0
+            
         isNegative = x < 0
         x = abs(x)
         hh = 0
